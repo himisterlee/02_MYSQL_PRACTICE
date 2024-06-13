596736de72ec2d7745df94d8f624774ab2cefc6b
himisterlee <lejlej100418@gmail.com>
2024-06-13 18:08:26 +0900

    문제 8 진행중

--- 02-MYSQL-PRACTICE/08.SUBQUERIES_PRACTICE.sql
+++ 02-MYSQL-PRACTICE/08.SUBQUERIES_PRACTICE.sql
@@ -18,11 +18,13 @@
 
 
         select
-            emp_name 이름,
-            job_code 직급코드,
-            salary 급여
+            a.emp_name 이름,
+            b.job_name 직급,
+            a.salary 급여
         from
-            employee
+            employee a
+        inner join
+                job b using(job_code)
         where
             dept_code = (
                 select
@@ -30,21 +32,10 @@
                 from
                     department
                 where
-                    dept_title = '해외영업1부'
+                    dept_id = 'D5'
                 );
 
 
-        select
-            a.emp_name 이름,
-            b.job_name 직급,
-            a.salary 급여
-        from
-            employee a,
-            job b
-        where
-            a.job_code = b.job_code
-        group by
-            b.job_name;
 
 -- EMPLOYEE 테이블에서 모든 직원의 평균 급여보다 높은 급여를 받는 직원의 이름과 급여를 조회하시오.
 
@@ -80,42 +71,29 @@
 
 -- EMPLOYEE 테이블에서 각 부서의 평균 급여보다 높은 급여를 받는 직원의 이름, 부서명, 급여를 조회하시오.
 
-        select
-            dept_code,
-            avg(salary)
-        from
-            employee
-        group by
-            dept_code;
 
         select
-            emp_name,
-            dept_code,
-            salary
+            a.emp_name 이름,
+            b.dept_title 부서명,
+            a.salary 급여
         from
-            employee
+            employee a
+        inner join
+                department b
+        on
+            a.dept_code = b.dept_id
         where
-            salary > (select avg(salary)
-                      from employee);
-
-
-        select
-            emp_name,
-            dept_code,
-            salary
-        from
-            employee
-        where
-            salary > (select avg(salary)
-                      from employee
-                      group by dept_code);
-
+        a.salary in (select avg(a.salary) < a.salary
+                        from employee
+                        group by b.dept_title);
 
 
 
 -- 상관서브쿼리문제
 -- EMPLOYEE 테이블에서 각 부서별로 가장 높은 급여를 받는 직원의 이름, 부서명, 급여를 조회하시오.
 
+    select
+        emp_name
 -- EMPLOYEE 테이블에서 각 직급별 평균 급여보다 높은 급여를 받는 직원의 이름, 직급, 급여를 조회하시오.
 
 
